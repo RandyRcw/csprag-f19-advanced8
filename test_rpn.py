48ac9b0c8b9fbb58f53b1df5a2d63eb501461130
Randall Wang <randyrcw@umich.edu>
2019-11-07 04:36:03 -0500

    Coverage should go down

--- test_rpn.py
+++ test_rpn.py
@@ -15,6 +15,6 @@ class TestBasics(unittest.TestCase):
     def test_divide(self):
         result = rpn.calculate("6 3 /")
         self.assertEqual(2, result)
-    def test_modulus(self):
-        result = rpn.calculate("87 18 %")
-        self.assertEqual(15, result)
+    # def test_modulus(self):
+    #     result = rpn.calculate("87 18 %")
+    #     self.assertEqual(15, result)
